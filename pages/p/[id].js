367fb693e04c66d92386e5b08da7f3e35d33accd
Andrew Kyllo <kyllo34@gmail.com>
2020-04-09 22:34:34 -0700

    completed lab

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,25 @@
-import { useRouter } from "next/router";
 import Layout from "../../components/MyLayout";
+const axios = require('axios').default;
 
-export default function Post() {
-  const router = useRouter();
+
+const Post = props => {
 
   return (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
+      <h1>{props.title}</h1>
+      <img src={props.image} alt={props.image.alt}/>
     </Layout>
   );
 }
+
+Post.getInitialProps = async function(context) {
+
+  const res = await axios(`http://xkcd.com/${context.query.id}/info.0.json`)
+  return {
+    title: res.data.title,
+    image: res.data.img,
+    alt: res.data.alt
+  }
+}
+
+export default Post;
